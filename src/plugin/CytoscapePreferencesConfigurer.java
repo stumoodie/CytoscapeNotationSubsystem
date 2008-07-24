1f54d2e4f26a1c2fde54b6899285299098df8f20
Richard Adams <richard.adams@ed.ac.uk>
2008-07-24 07:57:05 +0000

    move ui class to ui package

--- src/plugin/CytoscapePreferencesConfigurer.java
+++ src/plugin/CytoscapePreferencesConfigurer.java
@@ -1,9 +1,7 @@
-package uk.ac.ed.inf.csb.BasicCytoscape1_0_0.validation;
+package plugin;
 
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.pathwayeditor.application.contextadapter.uitoolkit.ValidationConfigPreferencesConfigurer;
-
-import plugin.Activator;
 
 public class CytoscapePreferencesConfigurer extends ValidationConfigPreferencesConfigurer {
 
